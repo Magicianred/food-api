071c87c2ba722cc0ff8096a9b20b230378a3e5c3
iivanabm <ivanammarinho@hotmail.com>
2021-01-23 16:04:29 -0300

    Tratamento de exceções globais

--- food-api/src/main/java/com/foodapi/api/controller/CidadeController.java
+++ food-api/src/main/java/com/foodapi/api/controller/CidadeController.java
@@ -1,14 +1,11 @@
 package com.foodapi.api.controller;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,7 +21,6 @@ import com.foodapi.domain.exception.NegocioException;
 import com.foodapi.domain.model.Cidade;
 import com.foodapi.domain.repository.CidadeRepository;
 import com.foodapi.domain.service.CadastroCidadeService;
-import com.foodapi.exceptionhandler.Problema;
 
 @RestController
 @RequestMapping(value = "/cidades")
@@ -75,29 +71,5 @@ public class CidadeController {
 	public void remover(@PathVariable Long cidadeId) {
 		cadastroCidade.excluir(cidadeId);
 	}
-	
-	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(
-			EntidadeNaoEncontradaException e){
 		
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem(e.getMessage()).build();
-			
-		return ResponseEntity.status(HttpStatus.NOT_FOUND)
-				.body(problema);
-	}
-	
-	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> tratarNegocioException(
-			NegocioException e){
-		
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem(e.getMessage()).build();
-		
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-				.body(problema);
-	}
-	
 }
