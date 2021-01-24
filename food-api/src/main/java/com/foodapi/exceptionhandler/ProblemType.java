85672be426de1b4f2c907113821a47299d587e4e
iivanabm <ivanammarinho@hotmail.com>
2021-01-24 16:46:45 -0300

    Formato do corpo da resposta padronizado com RFC 7807

---
+++ food-api/src/main/java/com/foodapi/exceptionhandler/ProblemType.java
@@ -0,0 +1,19 @@
+package com.foodapi.exceptionhandler;
+
+import lombok.Getter;
+
+@Getter
+public enum ProblemType {
+
+	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada."),
+	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso."),
+	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio.");
+	
+	private String title;
+	private String uri;
+	
+	ProblemType(String path, String title){
+		this.uri = "https://localhost:8080" + path;
+		this.title=title;
+	}
+}
