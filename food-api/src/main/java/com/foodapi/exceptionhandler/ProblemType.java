715b85a30df87fe7cd6c3b05976029b359fe2c32
iivanabm <ivanammarinho@hotmail.com>
2021-01-25 11:49:23 -0300

    Exceção NoHandlerFoundException tratada

--- food-api/src/main/java/com/foodapi/exceptionhandler/ProblemType.java
+++ food-api/src/main/java/com/foodapi/exceptionhandler/ProblemType.java
@@ -4,7 +4,8 @@ import lombok.Getter;
 
 @Getter
 public enum ProblemType {
-
+	
+	PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
 	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível."),
 	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada."),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso."),
