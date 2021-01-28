ae4563b83db654091bcbef9a432d0214359fdadd
iivanabm <ivanammarinho@hotmail.com>
2021-01-28 10:12:09 -0300

    Maven Failsafe Plugin

--- food-api/src/test/java/com/foodapi/CadastroCozinhaIT.java
+++ food-api/src/test/java/com/foodapi/CadastroCozinhaIT.java
@@ -10,33 +10,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.foodapi.domain.exception.CozinhaNaoEncontradaException;
+import com.foodapi.domain.exception.EntidadeEmUsoException;
 import com.foodapi.domain.model.Cozinha;
 import com.foodapi.domain.service.CadastroCozinhaService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CadastroCozinhaIntegrationTests {
+public class CadastroCozinhaIT {
 
 	@Autowired
 	private CadastroCozinhaService cadastroCozinha;
-	
+
 	@Test
-	public void testarCadastroCozinhaComSucesso() {
-		
+	public void deveAtribuirId_QuandoCadastrarCozinhaComDadosCorretos() {
+
 		Cozinha novaCozinha = new Cozinha();
 		novaCozinha.setNome("Chinesa");
-		
+
 		novaCozinha = cadastroCozinha.salvar(novaCozinha);
-		
+
 		assertThat(novaCozinha).isNotNull();
 		assertThat(novaCozinha.getId()).isNotNull();
 	}
-	
+
 	@Test(expected = ConstraintViolationException.class)
-	public void testarCadastroCozinhaSemNome() {
+	public void deveFalhar_QuandoCadastrarCozinhaSemNome() {
 		Cozinha novaCozinha = new Cozinha();
 		novaCozinha.setNome(null);
-		
-		novaCozinha = cadastroCozinha.salvar(novaCozinha);		
+
+		novaCozinha = cadastroCozinha.salvar(novaCozinha);
 	}
+
+	@Test(expected = EntidadeEmUsoException.class)
+	public void deveFalhar_QuandoExcluirCozinhaEmUso() {
+		cadastroCozinha.excluir(1L);
+	}
+
+	@Test(expected = CozinhaNaoEncontradaException.class)
+	public void deveFalhar_QuandoExcluirCozinhaInexistente() {
+		cadastroCozinha.excluir(100L);
+	}
+
 }
