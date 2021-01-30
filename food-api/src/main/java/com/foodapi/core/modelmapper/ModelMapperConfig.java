61a8530605eead55dc23e44610ec42b6e7a37340
iivanabm <ivanammarinho@hotmail.com>
2021-01-30 13:58:38 -0300

    Mapeamento customizado de propriedades com ModelMapper

--- food-api/src/main/java/com/foodapi/core/modelmapper/ModelMapperConfig.java
+++ food-api/src/main/java/com/foodapi/core/modelmapper/ModelMapperConfig.java
@@ -4,11 +4,19 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.foodapi.domain.model.Restaurante;
+import com.foodapi.model.RestauranteModel;
+
 @Configuration
 public class ModelMapperConfig {
 
 	@Bean
 	public ModelMapper modelMapper() {
-		return new ModelMapper();
+		var modelMapper = new ModelMapper();
+		
+		modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
+			.addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+		
+		return modelMapper;
 	}
 }
