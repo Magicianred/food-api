824e03d9be8925ccc4db32a333a97890c7e88b37
iivanabm <ivanammarinho@hotmail.com>
2021-01-30 19:37:12 -0300

    Mapeando para uma instância destino com ModelMapper

--- food-api/src/main/java/com/foodapi/assembler/RestauranteInputDisassembler.java
+++ food-api/src/main/java/com/foodapi/assembler/RestauranteInputDisassembler.java
@@ -4,6 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.foodapi.domain.model.Cozinha;
 import com.foodapi.domain.model.Restaurante;
 import com.foodapi.domain.model.input.RestauranteInput;
 
@@ -15,6 +16,13 @@ public class RestauranteInputDisassembler {
 
 	public Restaurante toDomainObject(RestauranteInput restauranteInput) {
 		return modelMapper.map(restauranteInput, Restaurante.class);
+	}
+	
+	public void copyToDomainObject(RestauranteInput restauranteInput, Restaurante restaurante) {
+		/* Para evitar org.springframework.orm.jpa.JpaSystemException: identifier of
+		   an instance of com.foodapi.domain.model.Cozinha was altered from 1 to 2*/
+		restaurante.setCozinha(new Cozinha());
+		modelMapper.map(restauranteInput, restaurante);
 	}
 
 }
