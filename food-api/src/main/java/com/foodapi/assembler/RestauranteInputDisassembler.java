fffa4d2d79b314657eb8b22f5e557bd867d65139
iivanabm <ivanammarinho@hotmail.com>
2021-01-30 13:25:15 -0300

    ModelMapper adicionado

--- food-api/src/main/java/com/foodapi/assembler/RestauranteInputDisassembler.java
+++ food-api/src/main/java/com/foodapi/assembler/RestauranteInputDisassembler.java
@@ -1,25 +1,20 @@
 package com.foodapi.assembler;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.foodapi.domain.model.Cozinha;
 import com.foodapi.domain.model.Restaurante;
 import com.foodapi.domain.model.input.RestauranteInput;
 
 @Component
 public class RestauranteInputDisassembler {
+	
+	@Autowired
+	private ModelMapper modelMapper;
 
 	public Restaurante toDomainObject(RestauranteInput restauranteInput) {
-		Restaurante restaurante = new Restaurante();
-		restaurante.setNome(restauranteInput.getNome());
-		restaurante.setTaxaFrete(restauranteInput.getTaxaFrete());
-
-		Cozinha cozinha = new Cozinha();
-		cozinha.setId(restauranteInput.getCozinha().getId());
-
-		restaurante.setCozinha(cozinha);
-
-		return restaurante;
+		return modelMapper.map(restauranteInput, Restaurante.class);
 	}
 
 }
--- food-api/src/main/java/com/foodapi/assembler/RestauranteModelAssembler.java
+++ food-api/src/main/java/com/foodapi/assembler/RestauranteModelAssembler.java
@@ -3,30 +3,25 @@ package com.foodapi.assembler;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.foodapi.domain.model.Restaurante;
-import com.foodapi.model.CozinhaModel;
 import com.foodapi.model.RestauranteModel;
 
 @Component
 public class RestauranteModelAssembler {
+	
+	@Autowired
+	private ModelMapper modelMapper;
 
 	public RestauranteModel toModel(Restaurante restaurante) {
-		CozinhaModel cozinhaModel = new CozinhaModel();
-		cozinhaModel.setId(restaurante.getCozinha().getId());
-		cozinhaModel.setNome(restaurante.getCozinha().getNome());
-		
-		RestauranteModel restauranteModel = new RestauranteModel();
-		restauranteModel.setId(restaurante.getId());
-		restauranteModel.setNome(restaurante.getNome());
-		restauranteModel.setTaxaFrete(restaurante.getTaxaFrete());
-		restauranteModel.setCozinha(cozinhaModel);
-		return restauranteModel;
+		return modelMapper.map(restaurante, RestauranteModel.class);	
 	}
 	
 	public List<RestauranteModel> toCollectionModel(List<Restaurante> restaurantes){
-		return restaurantes.parallelStream().map(restaurante -> toModel(restaurante))
+		return restaurantes.stream().map(restaurante -> toModel(restaurante))
 				.collect(Collectors.toList());
 	}
 }
