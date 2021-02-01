f27761de50f5fd15971aa676a7aeb695821af3a8
iivanabm <ivanammarinho@hotmail.com>
2021-02-01 11:20:19 -0300

    Endereço adicionado no modelo da representação do recurso de restaurante

--- food-api/src/main/java/com/foodapi/core/modelmapper/ModelMapperConfig.java
+++ food-api/src/main/java/com/foodapi/core/modelmapper/ModelMapperConfig.java
@@ -4,8 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.foodapi.domain.model.Restaurante;
-import com.foodapi.model.RestauranteModel;
+import com.foodapi.domain.model.Endereco;
+import com.foodapi.model.EnderecoModel;
 
 @Configuration
 public class ModelMapperConfig {
@@ -14,8 +14,14 @@ public class ModelMapperConfig {
 	public ModelMapper modelMapper() {
 		var modelMapper = new ModelMapper();
 		
-		modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
-			.addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+		/*modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
+			.addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);*/
+		
+		var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(Endereco.class, EnderecoModel.class);
+		
+		enderecoToEnderecoModelTypeMap.<String>addMapping(enderecoSrc -> enderecoSrc.getCidade()
+				.getEstado().getNome(), 
+				(enderecoModelDest, value) -> enderecoModelDest.getCidade().setEstado(value));
 		
 		return modelMapper;
 	}
