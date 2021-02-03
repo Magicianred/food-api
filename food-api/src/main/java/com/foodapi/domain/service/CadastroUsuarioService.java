77256063fbe413962f340250ff877087bb6397ef
iivanabm <ivanammarinho@hotmail.com>
2021-02-03 10:08:14 -0300

    Endpoints de associação de usuários com grupos

--- food-api/src/main/java/com/foodapi/domain/service/CadastroUsuarioService.java
+++ food-api/src/main/java/com/foodapi/domain/service/CadastroUsuarioService.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.foodapi.domain.exception.NegocioException;
 import com.foodapi.domain.exception.UsuarioNaoEncontradoException;
+import com.foodapi.domain.model.Grupo;
 import com.foodapi.domain.model.Usuario;
 import com.foodapi.domain.repository.UsuarioRepository;
 
@@ -15,6 +16,9 @@ public class CadastroUsuarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+    
+    @Autowired
+    private CadastroGrupoService cadastroGrupo;
        
     @Transactional
     public Usuario salvar(Usuario usuario) {
@@ -40,6 +44,23 @@ public class CadastroUsuarioService {
         
         usuario.setSenha(novaSenha);
     }
+    
+    @Transactional
+    public void desassociarGrupo(Long usuarioId, Long grupoId) {
+        Usuario usuario = buscarOuFalhar(usuarioId);
+        Grupo grupo = cadastroGrupo.buscarOuFalhar(grupoId);
+        
+        usuario.removerGrupo(grupo);
+    }
+
+    @Transactional
+    public void associarGrupo(Long usuarioId, Long grupoId) {
+        Usuario usuario = buscarOuFalhar(usuarioId);
+        Grupo grupo = cadastroGrupo.buscarOuFalhar(grupoId);
+        
+        usuario.adicionarGrupo(grupo);
+    }
+    
 
     public Usuario buscarOuFalhar(Long usuarioId) {
         return usuarioRepository.findById(usuarioId)
