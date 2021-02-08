f9e5d9def52cc87253f42ea69a586bf09e20839a
iivanabm <ivanammarinho@hotmail.com>
2021-02-08 10:18:52 -0300

    Consulta com dados agregados de vendas diárias

--- food-api/src/main/java/com/foodapi/infrastructure/service/VendaQueryServiceImpl.java
+++ food-api/src/main/java/com/foodapi/infrastructure/service/VendaQueryServiceImpl.java
@@ -1,17 +1,42 @@
 package com.foodapi.infrastructure.service;
 
+import java.util.Date;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
 import com.foodapi.domain.filter.VendaDiariaFilter;
+import com.foodapi.domain.model.Pedido;
 import com.foodapi.domain.model.dto.VendaDiaria;
 import com.foodapi.domain.service.VendaQueryService;
 
+@Repository
 public class VendaQueryServiceImpl implements VendaQueryService{
+	
+	@PersistenceContext
+	private EntityManager manager;
 
 	@Override
 	public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro) {
+		var builder = manager.getCriteriaBuilder();
+		var query = builder.createQuery(VendaDiaria.class);
+		var root = query.from(Pedido.class);		
 		
-		return null;
+		var functionDateDataCriacao = builder.function("date", Date.class,
+				root.get("dataCriacao"));
+		
+		var selection = builder.construct(VendaDiaria.class, 
+				functionDateDataCriacao, 
+				builder.count(root.get("id")),
+				builder.sum(root.get("valorTotal")));
+		
+		query.select(selection);
+		query.groupBy(functionDateDataCriacao);
+		
+		return manager.createQuery(query).getResultList();
 	}
 
 }
