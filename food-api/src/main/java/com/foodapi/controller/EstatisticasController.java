46211a7b910f8dd014299a883652100511bf14d2
iivanabm <ivanammarinho@hotmail.com>
2021-02-08 14:04:39 -0300

    Relatório JasperReports preenchido com JavaBeans e geração de bytes do PDF

--- food-api/src/main/java/com/foodapi/controller/EstatisticasController.java
+++ food-api/src/main/java/com/foodapi/controller/EstatisticasController.java
@@ -1,7 +1,9 @@
 package com.foodapi.controller;
 
+import org.springframework.http.HttpHeaders;
 import java.util.List;
-
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.foodapi.domain.filter.VendaDiariaFilter;
 import com.foodapi.domain.model.dto.VendaDiaria;
 import com.foodapi.domain.service.VendaQueryService;
+import com.foodapi.domain.service.VendaReportService;
 
 @RestController
 @RequestMapping(path = "/estatisticas")
@@ -19,11 +22,28 @@ public class EstatisticasController {
 	@Autowired
 	private VendaQueryService vendaQueryService;
 	
-	@GetMapping("/vendas-diarias")
+	@Autowired
+	private VendaReportService vendaReportService;
+	
+	@GetMapping(path = "/vendas-diarias", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro,
-			@RequestParam(required = false, defaultValue = "+00:00") String timeOffset){
+			@RequestParam(required = false, defaultValue = "+00:00") String timeOffset) {
 		return vendaQueryService.consultarVendasDiarias(filtro, timeOffset);
-		
-		
 	}
+	
+	@GetMapping(path = "/vendas-diarias", produces = MediaType.APPLICATION_PDF_VALUE)
+	public ResponseEntity<byte[]> consultarVendasDiariasPdf(VendaDiariaFilter filtro,
+			@RequestParam(required = false, defaultValue = "+00:00") String timeOffset) {
+		
+		byte[] bytesPdf = vendaReportService.emitirVendasDiarias(filtro, timeOffset);
+		
+		var headers = new HttpHeaders();
+		headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=vendas-diarias.pdf");
+		
+		return ResponseEntity.ok()
+				.contentType(MediaType.APPLICATION_PDF)
+				.headers(headers)
+				.body(bytesPdf);
+	}
+	
 }
---
+++ food-api/src/main/java/com/foodapi/domain/service/VendaReportService.java
@@ -0,0 +1,8 @@
+package com.foodapi.domain.service;
+
+import com.foodapi.domain.filter.VendaDiariaFilter;
+
+public interface VendaReportService {
+
+	byte[] emitirVendasDiarias(VendaDiariaFilter filtro, String timeOffset);
+}
