130963457c00d63e14a3f3562d1197fb14048639
iivanabm <ivanammarinho@hotmail.com>
2021-02-24 09:03:35 -0300

    CORS habilitado globalmente

--- food-api/src/main/java/com/foodapi/controller/RestauranteController.java
+++ food-api/src/main/java/com/foodapi/controller/RestauranteController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,7 +28,6 @@ import com.foodapi.domain.service.CadastroRestauranteService;
 import com.foodapi.model.RestauranteModel;
 import com.foodapi.model.view.RestauranteView;
 
-@CrossOrigin
 @RestController
 @RequestMapping(value = "/restaurantes")
 public class RestauranteController {
