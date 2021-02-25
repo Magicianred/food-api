9ce1f6a800fa69eccee8d9ee0ad5b4066b380aaa
iivanabm <ivanammarinho@hotmail.com>
2021-02-25 11:22:48 -0300

    Cache com cabeçalho Cache-Control e diretiva max-age

--- food-api/src/main/java/com/foodapi/controller/FormaPagamentoController.java
+++ food-api/src/main/java/com/foodapi/controller/FormaPagamentoController.java
@@ -1,6 +1,7 @@
 package com.foodapi.controller;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.validation.Valid;
 
@@ -14,7 +15,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import com.foodapi.assembler.FormaPagamentoInputDisassembler;
 import com.foodapi.assembler.FormaPagamentoModelAssembler;
 import com.foodapi.domain.model.FormaPagamento;
@@ -40,10 +44,15 @@ public class FormaPagamentoController {
     private FormaPagamentoInputDisassembler formaPagamentoInputDisassembler;
     
     @GetMapping
-    public List<FormaPagamentoModel> listar() {
+    public ResponseEntity<List<FormaPagamentoModel>> listar() {
         List<FormaPagamento> todasFormasPagamentos = formaPagamentoRepository.findAll();
         
-        return formaPagamentoModelAssembler.toCollectionModel(todasFormasPagamentos);
+        List<FormaPagamentoModel> formasPagamentosModel =  formaPagamentoModelAssembler
+        		.toCollectionModel(todasFormasPagamentos);
+        
+        return ResponseEntity.ok()
+        		.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+        		.body(formasPagamentosModel);
     }
     
     @GetMapping("/{formaPagamentoId}")
