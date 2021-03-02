7007283636ccd9d190b6797ec0cb580f15351ee1
iivanabm <ivanammarinho@hotmail.com>
2021-03-02 11:40:56 -0300

    Hypermedia em recursos com paginação

--- food-api/src/main/java/com/foodapi/model/CozinhaModel.java
+++ food-api/src/main/java/com/foodapi/model/CozinhaModel.java
@@ -1,14 +1,18 @@
 package com.foodapi.model;
 
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
+
 import com.fasterxml.jackson.annotation.JsonView;
 import com.foodapi.model.view.RestauranteView;
 
 import lombok.Getter;
 import lombok.Setter;
 
+@Relation(collectionRelation = "cozinhas")
 @Setter
 @Getter
-public class CozinhaModel {
+public class CozinhaModel extends RepresentationModel<CozinhaModel>{
 
 	@JsonView(RestauranteView.Resumo.class)
 	private Long id;
